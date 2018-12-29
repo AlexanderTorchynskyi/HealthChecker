cc4a77310e0c1e2663651540b46b293413f6b593
ФДучфтвукЕщксрнтілнш <9torchuncky6@gmail.com>
2018-12-29 14:47:21 +0200

    test api gateway

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 
 @RestController
 public class Controller {
+
+    private RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/")
     public String get() {
@@ -16,5 +20,13 @@ public class Controller {
     @GetMapping("/health")
     public String getTekst() {
         return "I'm alive";
+    }
+
+    @GetMapping("/api-gateway")
+    public ResponseEntity<String> getHealthApiGateway() {
+        System.out.println("Sending get to api-gateway");
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("be-api-gateway:8081/api/healthcheck", String.class);
+        System.out.println("Sending get to api-gateway: " + forEntity.getBody());
+        return forEntity;
     }
 }
